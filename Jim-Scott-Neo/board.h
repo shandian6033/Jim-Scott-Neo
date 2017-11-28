5179fbfc782190f827e46d18049ed1ae7e1fd173
Scott <shandian6043@gmail.com>
2017-11-28 11:38:05 -0500

    board.h second draft

--- Jim-Scott-Neo/board.h
+++ Jim-Scott-Neo/board.h
@@ -2,20 +2,25 @@
 #define BOARD_H
 #include "observer.h"
 #include "cell.h"
-#include <string>
+#include <iostream>
 
 //class TextDisplay;
 class Observer;
 
-class board :public Observer{
+class Board :public Observer{
 	std::vector<std::vector<Cell>> theBoard;
 	//TextDisplay *td = nullptr;
 	Observer *display = nullptr;
 public:
 	void init();
+	bool isOver();
 	void newBlock(int level);
 	void movement(std::string direction);
 	void notify(Subject &whoFrom) override;
+
+	~Board();
+
+	friend std::ostream &operator<<(std::ostream &out, const Board &b);
 };
 
 #endif // !BOARD_H
