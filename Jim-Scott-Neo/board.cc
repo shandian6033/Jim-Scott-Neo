df2a5d371dca0b077e9afc3baa56ca487879103c
JimmyLin1997 <33846109+JimmyLin1997@users.noreply.github.com>
2017-12-01 01:36:03 -0500

    好像push出了点问题，再做一遍

--- Jim-Scott-Neo/board.cc
+++ Jim-Scott-Neo/board.cc
@@ -2,7 +2,7 @@
 #include <cstdlib>
 
 void Board::init(int row, int col) {
-<<<<<<< HEAD
+
     length = row;
     width = col;
 
@@ -18,7 +18,6 @@ void Board::init(int row, int col) {
     for (int c = 0; c < col; c++) { //setup place_hoders
         place_hoders.emplace_back(Cell(-1, c));
     }
-
 
     score = 0;
     hi_score = 0;
@@ -51,42 +50,7 @@ void Board::init(int row, int col) {
             place_hoders.at(c).setRight(place_hoders.at(c + 1));
         }
     }
-=======
-	length = row;
-	width = col;
 
-	the_board.clear();
-	for (int r = 0; r < row; r++) {
-		std::vector<Cell> new_row;
-		for (int c = 0; c < col; c++) {
-			new_row.emplace_back(Cell(r, c));
-		}
-		the_board.emplace_back(new_row);
-	}
-	score = 0;
-	hi_score = 0;
-	//unique_ptr<Observer>ob = dynamic_cast<unique_ptr<Observer>>(td);
-	//td = new TextDisplay{ row,col };
-	td.clear();
-
-	for (int r = 0; r < row; r++) {
-		for (int c = 0; c < col; c++) {
-			the_board.at(r).at(c).attach(&td);
-			if (r > 0) {
-				the_board.at(r).at(c).setUp(the_board.at(r - 1).at(c));
-			}
-			if (r < row - 1) {
-				the_board.at(r).at(c).setDown(the_board.at(r + 1).at(c));
-			}
-			if (c > 0) {
-				the_board.at(r).at(c).setLeft(the_board.at(r).at(c - 1));
-			}
-			if (c < col - 1) {
-				the_board.at(r).at(c).setRight(the_board.at(r).at(c + 1));
-			}
-		}
-	}
->>>>>>> 761efc97eaa22f76a9e0607c38fc71c585bfe5ab
 }
 
 void Board::movement(std::string valid_cmd) {
