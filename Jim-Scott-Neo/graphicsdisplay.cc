8a8b0f6c0879c6d1d704e002d09d267a56749e74
JimmyLin1997 <33846109+JimmyLin1997@users.noreply.github.com>
2017-12-02 23:11:29 -0500

    update graphic

--- Jim-Scott-Neo/graphicsdisplay.cc
+++ Jim-Scott-Neo/graphicsdisplay.cc
@@ -5,55 +5,60 @@
 using namespace std;
 
 GraphicsDisplay::GraphicsDisplay(int width, int length, const Board& my_board)
-    :width{ width }, height{length},win_width {width*cell_size}, win_height{ (length + global_offset)*cell_size }, my_board{ &my_board }, xw{ win_width, win_height } {
+    :width{ width }, height{length},win_width {width*cell_size + 10}, win_height{ 5+(length + global_offset)*cell_size }, my_board{ &my_board }, xw{ win_width, win_height } {
     
 }
 
 void GraphicsDisplay::clear() {
     
+    xw.fillRectangle(0, 0, win_width, 5, Xwindow::Black); // top line
     xw.fillRectangle(0, 0, win_width, win_height, Xwindow::Background);
-    xw.fillRectangle(0, global_offset*cell_size - 5, win_width, 5,Xwindow::Black);
+    xw.fillRectangle(0, global_offset*cell_size - 5, win_width, 5,Xwindow::Black); //middle line
+    xw.fillRectangle(0, win_height-5, win_width, 5, Xwindow::Black);//bottom line
+
+    xw.fillRectangle(0, 0, 5, win_height, Xwindow::Black);//left line
+    xw.fillRectangle(win_width-5, 0, 5, win_height, Xwindow::Black);//right line
 }
 
 void GraphicsDisplay::notify(Subject &whoFrom) {
     Info info_I_got = whoFrom.getInfo();
     
     int x = info_I_got.col;
-    int y = info_I_got.row + global_offset;
+    int y = (info_I_got.row + global_offset);
 
     switch (info_I_got.id) {
     case WhoIam::L:
-        xw.fillRectangle(x * cell_size, y * cell_size, cell_size, cell_size, Xwindow::Red);
-        break;
-    case WhoIam::T:
-        xw.fillRectangle(x * cell_size, y * cell_size, cell_size, cell_size, Xwindow::Orange);
-        break;
-    case WhoIam::J:
-        xw.fillRectangle(x * cell_size, y * cell_size, cell_size, cell_size, Xwindow::Yellow);
-        break;
-    case WhoIam::Z:
-        xw.fillRectangle(x * cell_size, y * cell_size, cell_size, cell_size, Xwindow::Green);
-        break;
-    case WhoIam::O:
-        xw.fillRectangle(x * cell_size, y * cell_size, cell_size, cell_size, Xwindow::Cyan);
-        break;
-    case WhoIam::S:
-        xw.fillRectangle(x * cell_size, y * cell_size, cell_size, cell_size, Xwindow::Blue);
-        break;
-    case WhoIam::I:
-        xw.fillRectangle(x * cell_size, y * cell_size, cell_size, cell_size, Xwindow::Purple);
-        break;
-    case WhoIam::X:
-        xw.fillRectangle(x * cell_size, y * cell_size, cell_size, cell_size, Xwindow::DarkRed);
+        xw.fillRectangle(x*cell_size + 5, y*cell_size, cell_size, cell_size, Xwindow::Red);
+        break;            
+    case WhoIam::T:       
+        xw.fillRectangle(x*cell_size + 5, y*cell_size, cell_size, cell_size, Xwindow::Orange);
+        break;            
+    case WhoIam::J:       
+        xw.fillRectangle(x*cell_size + 5, y*cell_size, cell_size, cell_size, Xwindow::Yellow);
+        break;            
+    case WhoIam::Z:       
+        xw.fillRectangle(x*cell_size + 5, y*cell_size, cell_size, cell_size, Xwindow::Green);
+        break;            
+    case WhoIam::O:       
+        xw.fillRectangle(x*cell_size + 5, y*cell_size, cell_size, cell_size, Xwindow::Cyan);
+        break;            
+    case WhoIam::S:       
+        xw.fillRectangle(x*cell_size + 5, y*cell_size, cell_size, cell_size, Xwindow::Blue);
+        break;            
+    case WhoIam::I:       
+        xw.fillRectangle(x*cell_size + 5, y*cell_size, cell_size, cell_size, Xwindow::Purple);
+        break;            
+    case WhoIam::X:       
+        xw.fillRectangle(x*cell_size + 5, y*cell_size, cell_size, cell_size, Xwindow::DarkRed);
         break;
     case WhoIam::H:
-        xw.fillRectangle(x * cell_size, y * cell_size, cell_size, 3, Xwindow::Black);
-        xw.fillRectangle(x * cell_size, y * cell_size, 3, cell_size, Xwindow::Black);
-        xw.fillRectangle((x+1) * cell_size - 3, y* cell_size, 3, cell_size, Xwindow::Black);
-        xw.fillRectangle(x * cell_size, (y+1) * cell_size - 3,cell_size, 3, Xwindow::Black);
+        xw.fillRectangle(x * cell_size+5 , y * cell_size, cell_size, 3, Xwindow::Black);
+        xw.fillRectangle(x * cell_size + 5, y * cell_size, 3, cell_size, Xwindow::Black);
+        xw.fillRectangle((x+1) * cell_size - 3 + 5, y* cell_size, 3, cell_size, Xwindow::Black);
+        xw.fillRectangle(x * cell_size + 5, (y+1) * cell_size - 3,cell_size, 3, Xwindow::Black);
         break;
     default:
-        xw.fillRectangle(x * cell_size, y * cell_size, cell_size, cell_size, Xwindow::Background);
+        xw.fillRectangle(x * cell_size + 5, y * cell_size, cell_size, cell_size, Xwindow::Background);
     }
 }
 
@@ -93,9 +98,9 @@ void GraphicsDisplay::updateText(){
     string hi_score_str = "Hi Score: " + to_string(my_board->getHi());
     next_block_str = "Next:";
 
-    xw.fillRectangle(0, 0, win_width, global_offset*cell_size-5,Xwindow::Background); //rewrite window
+    xw.fillRectangle(5, 10, win_width-10, global_offset*cell_size-10,Xwindow::Background); //clean window
 
-    int offset = 5;
+    int offset = 15;
     int v_space = 30;
     xw.drawString(offset, v_space, lev_str);
     xw.drawString(offset, 2*v_space, score_str);
